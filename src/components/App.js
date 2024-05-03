574f885b3ee8a89e6c8390f3ef4e2e820947e96b
joy <blackbearmode1@gmail.com>
2024-05-03 07:39:10 +0300

    create

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [dogImage, setDogImage] = useState(null);
+    const [loading, setLoading]  = useState(true);
+    
+    useEffect(() => {
+        async function fetchDogImage() {
+            setLoading(true);
+            const response = await fetch("https://dog.ceo/api/breeds/image/random");
+            const data = await response.json();
+            setDogImage(data.message);
+            setLoading(false);
+        }
+      fetchDogImage();  
+}, [];
+
+return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}  
+    </div>
+);
+}
+
+export default App;
